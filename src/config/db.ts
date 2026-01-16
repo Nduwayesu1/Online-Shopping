41ddeeebc3d9a430178701679ac23b4355d10dd9
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2026-01-15 21:33:29 -0800

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,14 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL, // <- use Render's internal URL
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
-
 
 const connectDB = async (): Promise<void> => {
   try {
