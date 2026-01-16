88aed15e13eeacfac7183b0002ede643c8fe37e4
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2026-01-15 21:05:45 -0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,28 @@
 import express from "express";
-import Products from "./routes/Products.js";
-import Categories from "./routes/Category.js";
-import Carts from "./routes/Cart.js";
-import Users from "./routes/Users.js";
-import connectDB, { pool } from "./config/db.js";
-import {swaggerSetup} from "./swager.js"
+import Products from "./routes/Products";
+import Categories from "./routes/Category";
+import Carts from "./routes/Cart";
+import Users from "./routes/Users";
+import connectDB, { pool } from "./config/db";
+import { setupSwagger } from "./swager";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 
-// Root route
 app.get("/", (_req, res) => {
   res.send("Welcome to Klab Academy");
 });
 
-// API Routes
 app.use("/api/products", Products);
 app.use("/api/categories", Categories);
 app.use("/api/carts", Carts);
 app.use("/api/users", Users);
 
-
 setupSwagger(app);
-
 
 app.listen(PORT, async () => {
   await pool.connect();
-  console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+  console.log(`Server running on port ${PORT}`);
 });
